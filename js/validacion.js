2e093c5f467744eadc5df4f21596ed217fa71ca3
camilabellas <camibellas22@gmail.com>
2023-08-10 09:56:07 -0300

    Actividad finalizada

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,23 @@
+document.getElementById("registrationForm").addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const password= document.getElementById('password1').value;
+    const repeatPassword = document.getElementById('password2').value; 
+    const checkbox = document.getElementById('terminos').checked;
+
+    
+    if ((password !== repeatPassword) || (password1.length < 6) || (!checkbox)) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+});
+
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    document.getElementById("alert-danger").classList.add("show"); 
 }
